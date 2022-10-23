68a528270cd0ddec1eb4e412ee5f387b4f394e2f
Cem DOŞ <kapawebtasarim@gmail.com>
2022-10-23 13:54:33 +0300

    Add project files.

---
+++ AriBilgi.Blog.UI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AriBilgi.Blog.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AriBilgi.Blog.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AriBilgi.Blog.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
