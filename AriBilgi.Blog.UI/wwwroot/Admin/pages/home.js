bd0b5c50d3f488a59c0e86c4efd5c2a3289c919d
Cem DOŞ <kapawebtasarim@gmail.com>
2022-10-30 10:20:49 +0300

    .

---
+++ AriBilgi.Blog.UI/wwwroot/Admin/pages/home.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('HomeApp', []);
+
+app.controller("HomeController", function ($scope, $http) {
+
+    $scope.UserList = WebApi.Result;
+
+  
+
+    
+});
+
+
+var html = "<p>Uğurcan</p>";
+html += "<p>Nedim</p>";
+html += "<p>Attilla</p>";
+$("#users").html(html);
+
+
